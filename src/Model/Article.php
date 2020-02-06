da9afce13bd87fdd0ec632100c795a7aa91b4e53
Randazzo Pierre <pierre.randazzo@viacesi.fr>
2020-02-06 09:51:32 +0100

    correction moteur recherche 2

--- src/Model/Article.php
+++ src/Model/Article.php
@@ -21,7 +21,7 @@ class Article extends Contenu implements \JsonSerializable
     {
         try {
             $searchA = $_GET['search'];
-            $requete = $bdd->prepare('SELECT * FROM articles WHERE Titre OR Auteur LIKE :search');
+            $requete = $bdd->prepare('SELECT * FROM articles WHERE Titre  LIKE :search OR Auteur LIKE :search');
             $requete->execute([
                 'search' => '%'.$searchA.'%'
             ]);
